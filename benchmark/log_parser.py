803d0770e173fa0da3e434b9bf137be235265a71
Ramil Safin <RamilSafNab1996@gmail.com>
2018-05-09 15:13:36 +0300

    Parse log info into a dict
    Parse log information into a dict instead of a tuple.
    This is done in order to ease client and server logs joining operation.

--- benchmark/log_parser.py
+++ benchmark/log_parser.py
@@ -13,7 +13,7 @@ def retrieve_client_qos_stats(logs):
 
 
 def parse_client_bench_trials(trials):
-    """Parses client benchmark's statistics into the resulting list of tuples - (trial_num, {statistics dict})"""
+    """Parses client benchmark's statistics into the resulting list of dicts"""
     res_lst = []
     for trial in trials:
         res_lst.append(parse_client_bench_trial(trial))
@@ -27,9 +27,10 @@ def parse_client_bench_trial(trial):
                       r'.*inter_packet_gap_ms_min\s(.*?)\n.*inter_packet_gap_ms_ave\s(.*?)\n'
                       r'.*inter_packet_gap_ms_max\s(.*?)\n', trial, re.S).groups()
 
-    return groups[0], {'num_packets_received': groups[1], 'num_packets_lost': groups[2], 'bitrate_min': groups[3],
-                       'bitrate_avg': groups[4], 'bitrate_max': groups[5], 'inter_packet_gap_min': groups[6],
-                       'inter_packet_gap_avg': groups[7], 'inter_packet_gap_max': groups[8]}
+    return {'trial': groups[0], 'num_packets_received': groups[1], 'num_packets_lost': groups[2],
+            'bitrate_min': groups[3], 'bitrate_avg': groups[4], 'bitrate_max': groups[5],
+            'inter_packet_gap_min': groups[6], 'inter_packet_gap_avg': groups[7],
+            'inter_packet_gap_max': groups[8]}
 
 
 def retrieve_server_bench_info(logs):
@@ -38,7 +39,7 @@ def retrieve_server_bench_info(logs):
 
 
 def parse_server_trials(trial_entries):
-    """Parses each server bench trial and returns resulting list of tuples - (trial_num, {info dict})"""
+    """Parses each server bench trial and returns resulting list of dicts"""
     res_lst = []
     for e in trial_entries:
         res_lst.append(parse_server_trial(e))
@@ -51,17 +52,19 @@ def parse_server_trial(e):
                       r'.*Max NALU size:\s(\d+)'
                       r'.*x265 \[info\]: frame I:.*, Avg QP:(.*?)\skb/s: (.*?)'
                       r'\n', e, re.S).groups()
-    return groups[0], {'fps': groups[1], 'bitrate': groups[2], 'datagram_size': groups[3], 'NALU': groups[4],
-                       'qp': groups[5], 'codec_bitrate': groups[6]}
+    return {'trial': groups[0], 'fps': groups[1], 'bitrate': groups[2], 'datagram_size': groups[3],
+            'NALU': groups[4],'qp': groups[5], 'codec_bitrate': groups[6]}
 
 
 if __name__ == '__main__':
     with open(SERVER_LOG_FILE, 'r', FILE_IO_BUF_SIZE) as server_log:
         server_logs = retrieve_server_bench_info(server_log.read())
 
+    print parse_server_trials(server_logs)[:1]
     print parse_server_trials(server_logs)
 
     with open(CLIENT_LOG_FILE, 'r', FILE_IO_BUF_SIZE) as client_log:
         qos_stats = retrieve_client_qos_stats(client_log.read())
 
+    print parse_client_bench_trials(qos_stats)[:1]
     print parse_client_bench_trials(qos_stats)
